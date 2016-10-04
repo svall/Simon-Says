392599d69aa63f4deaacc487bf6f33fe5d9721b2
Susana Isaza <susisaza@gmail.com>
2016-10-04 17:30:37 -0400

    completed page layout

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   console.log('DOM Loaded ok!');
+
+  $('#startGameBtn').click(shuffleColor);
 });
+
+function shuffleColor() {
+  console.log('startGameBtn working');
+  var color1 = $('#box1');
+  var color2 = $('#box2');
+  var color3 = $('#box3');
+  var color4 = $('#box4');
+
+
+
+
+}
