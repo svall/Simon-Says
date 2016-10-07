10990b4febb92d0d10e00ecc73c556eaf42b5d61
Susana Isaza <susisaza@gmail.com>
2016-10-06 20:37:54 -0400

    browser resizing changes

--- script.js
+++ script.js
@@ -62,10 +62,14 @@ function setBoard() {
     // console.log(colorsArray);
   }
   if($('#modeOptions').text() === 'beginner') {
-    $('#colorContainer').css('width', '45%');
+    $('#colorContainer').css('width', '60%');
+    $('.colorBox').css({
+      height: '180px',
+      width: '180px'
+    });
   }
   if(($('#modeOptions').text() === 'intermediate') || ($('#modeOptions').text() === 'extreme')) {
-    $('#colorContainer').css('width', '60%');
+    $('#colorContainer').css('width', '50%');
   }
   if($('#modeOptions').text() === 'extreme') {
     $('.timerDisplay').css('display', 'in-line');
